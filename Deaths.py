0ff6f513b1eafd81ac715da14295c03a060bc3cf
Vitor Noguchi <vshojifn@gmail.com>
2020-04-30 11:23:13 -0300

    Update Deaths.py

--- Deaths.py
+++ Deaths.py
@@ -8,77 +8,87 @@ class DeathsCsv:
         print("abrir csv")
 
     def csv(self):
-        corona_cases = {'wld_pt': [], 'br_pt': [], 'sp_pt': [], 'us_pt': [], 'it_pt': [], 'dt_pt': [], 'dt_lbl_pt': [],
-                        'dt_lbl_loc': [], 'wld_ylbl_loc': [], 'ctry_ylbl_loc': [], 'wld_lbl': [],'ctry_lbl': []}
+        color = ['black','yellow', 'red', 'blue', 'green', 'orange', 'purple', 'pink', 'brown', 'grey']
+        ctry_list = ['world', 'brazil', 'spain', 'us', 'italy','teste']
+        cases_svg = {'ctry_list': ctry_list, 'ctry_pt': [], 'ctry_len': [], 'dt_pt': [], 'dt_lbl_pt': [], 'dt_lbl_loc': [],
+                     'wld_ylbl_loc': [], 'ctry_ylbl_loc': [], 'wld_lbl': [],'ctry_lbl': [], 'color': color}
+        cases = {'wmax': [], 'cmax': [],'dt_lbl': [], 'diff_dt': [], 'dt_count': []}
 
-        wld_dth = []; br_dth = []; sp_dth = []; us_dth = []; it_dth = []; dt = []; diff_dt = []; dt_count = [0]
-
-        for pais in ['world', 'brazil', 'spain', 'us', 'italy']:
-            with open(f'/home/semantix/PycharmProjects/Desafio4/{pais}.csv','r') as csvfile:
-                readCSV = csv.reader(csvfile, delimiter=',')
-                for row in readCSV:
-                    if row[0] != 'Country':
-                        if pais == 'world':
-                            wld_dth.append(row[2].replace('.', ''))
-                        elif pais == 'brazil':
-                            br_dth.append(row[2].replace('.',''))
+        for pais in ctry_list:
+            dt_lbls = []; ctry = []; dt = []; diff_dt = []; dt_count = []
+            try:
+                dt_count.append(0)
+                with open(f'/home/semantix/PycharmProjects/Desafio4/{pais}.csv','r') as csvfile:
+                    readCSV = csv.reader(csvfile, delimiter=',')
+                    for row in readCSV:
+                        if row[0] != 'Country':
+                            ctry.append(row[2].replace('.', ''))
                             dt.append(row[4])
-                        elif pais == 'spain':
-                            sp_dth.append(row[2].replace('.', ''))
-                        elif pais == 'us':
-                            us_dth.append(row[2].replace('.', ''))
-                        else:
-                            it_dth.append(row[2].replace('.', ''))
-                csvfile.close()
-
-        for a in range(0, len(dt)):
-            b = datetime.strptime(dt[a], '%d/%m/%Y %H:%M') - datetime.strptime(dt[0], '%d/%m/%Y %H:%M')
-            e = int(b.total_seconds()/60)
-            diff_dt.append(e)
-            corona_cases['dt_lbl_pt'].append(dt[a].split(' ')[0])
-
-        count = 0
-
-        for w in range(0,len(corona_cases['dt_lbl_pt'])-1):
-            if corona_cases['dt_lbl_pt'][w] == corona_cases['dt_lbl_pt'][w+1]:
-                count += 1
+                    csvfile.close()
+            except:
+                print('arquivo invalido')                            #CRIAR LOG
+            if pais == 'world':
+                cases['wmax'].append(ctry[-1])
             else:
-                count += 1
-                dt_count.append(count)
+                cases['cmax'].append(ctry[-1])
+            for a in range(0, len(dt)):
+                b = datetime.strptime(dt[a], '%d/%m/%Y %H:%M') - datetime.strptime(dt[1], '%d/%m/%Y %H:%M')
+                e = int(b.total_seconds()/60)
+                diff_dt.append(e)
+                dt_lbls.append(dt[a].split(' ')[0])
 
-        corona_cases['dt_lbl_pt']= list(dict.fromkeys(corona_cases['dt_lbl_pt']))
-        ymax = max(int(us_dth[-1]), int(it_dth[-1]), int(sp_dth[-1]), int(br_dth[-1]), key=int)
+            count = 0
+            for w in range(0,len(dt_lbls)-1):
+                if dt_lbls[w] == dt_lbls[w+1]:
+                    count += 1
+                else:
+                    count += 1
+                    dt_count.append(count)
 
+            dt_lbl = list(dict.fromkeys(dt_lbls))
+            cases_svg['ctry_pt'].append(ctry)
+            cases_svg['dt_pt'].append(dt)
+            cases_svg['dt_lbl_pt'].append(dt_lbl)
+            cases['diff_dt'].append(diff_dt)
+            cases['dt_count'].append(dt_count)
 
-        for x in range(0,len(br_dth)):
-            corona_cases['wld_pt'].append(420 - 358 * (int(wld_dth[x]) / int(wld_dth[-1])))
-            corona_cases['br_pt'].append(420 - 358 * (int(br_dth[x]) / int(ymax)))
-            corona_cases['sp_pt'].append(420 - 358 * (int(sp_dth[x]) / int(ymax)))
-            corona_cases['us_pt'].append(420 - 358 * (int(us_dth[x]) / int(ymax)))
-            corona_cases['it_pt'].append(420 - 358 * (int(it_dth[x]) / int(ymax)))
-            corona_cases['dt_pt'].append(615 * (diff_dt[x]/diff_dt[-1]) + 190)
+        ymax = max(cases['cmax'], key=int)
+        for w in range(0,len(cases_svg['ctry_pt'][0])):
+            cases_svg['ctry_pt'][0][w] = (420 - (358 * (int(cases_svg['ctry_pt'][0][w]) / int(cases['wmax'][0]))))
 
-        if (int(int(wld_dth[-1]) / 100000) - int(int(wld_dth[-1]) / 100000)) >= 0.5:
-            for wz in range(1, (int(int(wld_dth[-1]) / 100000) + 2)):
-                corona_cases['wld_lbl'].append(wz*100)
-                corona_cases['wld_ylbl_loc'].append(420 - wz * 100000 * (358 / int(wld_dth[-1])))
+        for x in range(1,len(cases_svg['ctry_pt'])):
+            for z in range(0,len(cases_svg['ctry_pt'][x])):
+                cases_svg['ctry_pt'][x][z] = (420 - 358 * (int(cases_svg['ctry_pt'][x][z]) / int(ymax)))
+        for dd in range(0,len(cases_svg['dt_pt'])):
+            for d in range(0,len(cases_svg['dt_pt'][dd])):
+                cases_svg['dt_pt'][dd][d] = (615 * (cases['diff_dt'][dd][d] / cases['diff_dt'][dd][-1]) + 190)
+
+        if ((int(cases['wmax'][0]) / 1000000) -  (int(int(cases['wmax'][0]) / 1000000))) >= 0.5:
+            for wz in range(0, (int(int(cases['wmax'][0]) / 100000)) + 2):
+                cases_svg['wld_lbl'].append(wz*100)
+                cases_svg['wld_ylbl_loc'].append(420 - wz * 100000 * (358 / int(cases['wmax'][0])))
         else:
-            for wz in range(1, (int(int(wld_dth[-1]) / 100000) + 2)):
-                corona_cases['wld_lbl'].append(wz*100)
-                corona_cases['wld_ylbl_loc'].append(420 - wz * 100000 * (358 / int(wld_dth[-1])))
-        if (int(ymax) / 10000) - int(int(ymax) / 10000) >= 0.5:
-            for cz in range(1, (int(int(ymax) / 10000) + 2)):
-                corona_cases['ctry_lbl'].append(cz*10)
-                corona_cases['ctry_ylbl_loc'].append(420 - cz * 10000 * (358 / int(ymax)))
+            for wz in range(0, (int(int(cases['wmax'][0]) / 100000) + 1)):
+                cases_svg['wld_lbl'].append(wz*100)
+                cases_svg['wld_ylbl_loc'].append(420 - wz * 100000 * (358 / int(cases['wmax'][0])))
+
+        if ((int(ymax) / 200000) - int(int(ymax) / 10000)) >= 0.5:
+            for cz in range(0, (int(int(ymax) / 10000) + 2)):
+                cases_svg['ctry_lbl'].append(cz*10)
+                cases_svg['ctry_ylbl_loc'].append(420 - cz * 10000 * (358 / int(ymax)))
         else:
-            for cz in range(1, (int(int(ymax) / 10000) + 1)):
-                corona_cases['ctry_lbl'].append(cz * 10)
-                corona_cases['ctry_ylbl_loc'].append(420 - cz * 10000 * (358 / int(ymax)))
-        for dl in dt_count:
-            corona_cases['dt_lbl_loc'].append(corona_cases['dt_pt'][dl])
+            for cz in range(0, (int(int(ymax) / 10000) + 1)):
+                cases_svg['ctry_lbl'].append(cz * 10)
+                cases_svg['ctry_ylbl_loc'].append(420 - cz * 10000 * (358 / int(ymax)))
 
+        for ddl in range(0, len(cases_svg['dt_pt'])):
+            for dl in cases['dt_count'][ddl]:
+                cases_svg['dt_lbl_loc'].append(cases_svg['dt_pt'][ddl][dl])
+                cases_svg['dt_lbl_loc'] = list(dict.fromkeys(cases_svg['dt_lbl_loc']))
 
-        return corona_cases
-
-if __name__ == "__main__":
-    a = DeathsCsv().csv()
+        ctry_len = []
+        for a in range(0, len(cases_svg['ctry_list'])):
+            ctry_len.append(len(cases_svg['ctry_pt'][a]))
+        cases_svg['ctry_len'].append(ctry_len)
+        
+        return cases_svg
