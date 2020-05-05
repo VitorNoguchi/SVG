77e4e48bf9c49ddf43f5ddde298ce525c7f3ac21
Vitor Noguchi <vshojifn@gmail.com>
2020-05-05 17:22:09 -0300

    Update Recovered.py

--- Recovered.py
+++ Recovered.py
@@ -10,9 +10,9 @@ class RecoveredCsv:
     def csv(self):
         color = ['black','yellow', 'red', 'blue', 'green', 'orange', 'purple', 'pink', 'brown', 'grey']
         ctry_list = ['world', 'brazil', 'spain', 'us', 'italy','teste']
-        cases_svg = {'ctry_list': ctry_list, 'ctry_pt': [], 'ctry_len': [], 'dt_pt': [], 'dt_lbl_pt': [], 'dt_lbl_loc': [],
+        recovered_svg = {'ctry_list': ctry_list, 'ctry_pt': [], 'ctry_len': [], 'dt_pt': [], 'dt_lbl_pt': [], 'dt_lbl_loc': [],
                      'wld_ylbl_loc': [], 'ctry_ylbl_loc': [], 'wld_lbl': [],'ctry_lbl': [], 'color': color}
-        cases = {'wmax': [], 'cmax': [],'dt_lbl': [], 'diff_dt': [], 'dt_count': []}
+        recovered = {'wmax': [], 'cmax': [],'dt_lbl': [], 'diff_dt': [], 'dt_count': []}
 
         for pais in ctry_list:
             dt_lbls = []; ctry = []; dt = []; diff_dt = []; dt_count = []
@@ -28,9 +28,9 @@ class RecoveredCsv:
             except:
                 print('arquivo invalido')                            #CRIAR LOG
             if pais == 'world':
-                cases['wmax'].append(ctry[-1])
+                recovered['wmax'].append(ctry[-1])
             else:
-                cases['cmax'].append(ctry[-1])
+                recovered['cmax'].append(ctry[-1])
             for a in range(0, len(dt)):
                 b = datetime.strptime(dt[a], '%d/%m/%Y %H:%M') - datetime.strptime(dt[1], '%d/%m/%Y %H:%M')
                 e = int(b.total_seconds()/60)
@@ -46,52 +46,52 @@ class RecoveredCsv:
                     dt_count.append(count)
 
             dt_lbl = list(dict.fromkeys(dt_lbls))
-            cases_svg['ctry_pt'].append(ctry)
-            cases_svg['dt_pt'].append(dt)
-            cases_svg['dt_lbl_pt'].append(dt_lbl)
-            cases['diff_dt'].append(diff_dt)
-            cases['dt_count'].append(dt_count)
+            recovered_svg['ctry_pt'].append(ctry)
+            recovered_svg['dt_pt'].append(dt)
+            recovered_svg['dt_lbl_pt'].append(dt_lbl)
+            recovered['diff_dt'].append(diff_dt)
+            recovered['dt_count'].append(dt_count)
 
-        ymax = max(cases['cmax'], key=int)
-        for w in range(0,len(cases_svg['ctry_pt'][0])):
-            cases_svg['ctry_pt'][0][w] = (420 - (358 * (int(cases_svg['ctry_pt'][0][w]) / int(cases['wmax'][0]))))
+        ymax = max(recovered['cmax'], key=int)
+        for w in range(0,len(recovered_svg['ctry_pt'][0])):
+            recovered_svg['ctry_pt'][0][w] = (420 - (358 * (int(recovered_svg['ctry_pt'][0][w]) / int(recovered['wmax'][0]))))
 
-        for x in range(1,len(cases_svg['ctry_pt'])):
-            for z in range(0,len(cases_svg['ctry_pt'][x])):
-                cases_svg['ctry_pt'][x][z] = (420 - 358 * (int(cases_svg['ctry_pt'][x][z]) / int(ymax)))
-        for dd in range(0,len(cases_svg['dt_pt'])):
-            for d in range(0,len(cases_svg['dt_pt'][dd])):
-                cases_svg['dt_pt'][dd][d] = (615 * (cases['diff_dt'][dd][d] / cases['diff_dt'][dd][-1]) + 190)
+        for x in range(1,len(recovered_svg['ctry_pt'])):
+            for z in range(0,len(recovered_svg['ctry_pt'][x])):
+                recovered_svg['ctry_pt'][x][z] = (420 - 358 * (int(recovered_svg['ctry_pt'][x][z]) / int(ymax)))
+        for dd in range(0,len(recovered_svg['dt_pt'])):
+            for d in range(0,len(recovered_svg['dt_pt'][dd])):
+                recovered_svg['dt_pt'][dd][d] = (615 * (recovered['diff_dt'][dd][d] / recovered['diff_dt'][dd][-1]) + 190)
 
-        if ((int(cases['wmax'][0]) / 100000) -  (int(int(cases['wmax'][0]) / 100000))) >= 0.5:
-            for wz in range(0, (int(int(cases['wmax'][0]) / 200000)) + 2):
-                cases_svg['wld_lbl'].append(round(wz*0.2, 1))
-                cases_svg['wld_ylbl_loc'].append(420 - wz * 200000 * (358 / int(cases['wmax'][0])))
+        if ((int(recovered['wmax'][0]) / 100000) -  (int(int(recovered['wmax'][0]) / 100000))) >= 0.5:
+            for wz in range(0, (int(int(recovered['wmax'][0]) / 200000)) + 2):
+                recovered_svg['wld_lbl'].append(round(wz*0.2, 1))
+                recovered_svg['wld_ylbl_loc'].append(420 - wz * 200000 * (358 / int(recovered['wmax'][0])))
         else:
-            for wz in range(0, (int(int(cases['wmax'][0]) / 200000) + 1)):
-                cases_svg['wld_lbl'].append(round(wz*0.2, 1))
-                cases_svg['wld_ylbl_loc'].append(420 - wz * 200000 * (358 / int(cases['wmax'][0])))
+            for wz in range(0, (int(int(recovered['wmax'][0]) / 200000) + 1)):
+                recovered_svg['wld_lbl'].append(round(wz*0.2, 1))
+                recovered_svg['wld_ylbl_loc'].append(420 - wz * 200000 * (358 / int(recovered['wmax'][0])))
 
         if ((int(ymax) / 200000) - int(int(ymax) / 200000)) >= 0.5:
             for cz in range(0, (int(int(ymax) / 20000) + 2)):
-                cases_svg['ctry_lbl'].append(cz*20)
-                cases_svg['ctry_ylbl_loc'].append(420 - cz * 20000 * (358 / int(ymax)))
+                recovered_svg['ctry_lbl'].append(cz*20)
+                recovered_svg['ctry_ylbl_loc'].append(420 - cz * 20000 * (358 / int(ymax)))
         else:
             for cz in range(0, (int(int(ymax) / 200000) + 1)):
-                cases_svg['ctry_lbl'].append(cz * 20)
-                cases_svg['ctry_ylbl_loc'].append(420 - cz * 20000 * (358 / int(ymax)))
+                recovered_svg['ctry_lbl'].append(cz * 20)
+                recovered_svg['ctry_ylbl_loc'].append(420 - cz * 20000 * (358 / int(ymax)))
 
-        for ddl in range(0, len(cases_svg['dt_pt'])):
-            for dl in cases['dt_count'][ddl]:
-                cases_svg['dt_lbl_loc'].append(cases_svg['dt_pt'][ddl][dl])
-                cases_svg['dt_lbl_loc'] = list(dict.fromkeys(cases_svg['dt_lbl_loc']))
+        for ddl in range(0, len(recovered_svg['dt_pt'])):
+            for dl in recovered['dt_count'][ddl]:
+                recovered_svg['dt_lbl_loc'].append(recovered_svg['dt_pt'][ddl][dl])
+                recovered_svg['dt_lbl_loc'] = list(dict.fromkeys(recovered_svg['dt_lbl_loc']))
 
         ctry_len = []
-        for a in range(0, len(cases_svg['ctry_list'])):
-            ctry_len.append(len(cases_svg['ctry_pt'][a]))
-        cases_svg['ctry_len'].append(ctry_len)
+        for a in range(0, len(recovered_svg['ctry_list'])):
+            ctry_len.append(len(recovered_svg['ctry_pt'][a]))
+        recovered_svg['ctry_len'].append(ctry_len)
 
-        return cases_svg
+        return recovered_svg
 
 if __name__ == "__main__":
-    a = CasesCsv().csv()
+    a = RecoveredCsv().csv()
