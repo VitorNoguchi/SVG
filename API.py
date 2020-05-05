1c9aa3480eaabc5139ee307e60ca4c1bfbca2fdd
Vitor Noguchi <vshojifn@gmail.com>
2020-05-05 17:20:59 -0300

    Update API.py

--- API.py
+++ API.py
@@ -5,38 +5,41 @@ from flask import Flask, render_template
 import Cases
 import Deaths
 import Recovered
-import cs
 
 CS = Cases.CasesCsv()
 dth = Deaths.DeathsCsv()
 rcv = Recovered.RecoveredCsv()
-cases_par = cs.CasesCsv()
 app = Flask(__name__)
 
 @app.route('/cases', methods=['GET', 'POST'])
 def foo():
-    cs_data = CS.csv()
-    return render_template('cases.html', dic_cs= cs_data, len_cs=len(cs_data['br_pt']), world_label_len_cs = len(cs_data['wld_lbl']),
-                           country_label_len_cs = len(cs_data['ctry_lbl']), dates_label_len_cs= len(cs_data['dt_lbl_loc']))
+    cs_dataa = CS.csv()
+    ctry_len = []
+    for a in range(0,len(cs_dataa['ctry_list'])):
+        ctry_len.append(len(cs_dataa['ctry_pt'][a]))
+    return render_template('cases.html', dic= cs_dataa, ctry_count= len(cs_dataa['ctry_list']), ctry_len= ctry_len,
+                           wld_lbl_len = len(cs_dataa['wld_lbl']), ctry_lbl_len = len(cs_dataa['ctry_lbl']),
+                           dt_lbl_len= len(cs_dataa['dt_lbl_loc']))
 
 @app.route('/deaths', methods=['GET', 'POST'])
 def foo1():
     dth_data = dth.csv()
-    return render_template('deaths.html', dic_dth= dth_data, len_dth=len(dth_data['br_pt']), world_label_len_dth = len(dth_data['wld_lbl']),
-                           country_label_len_dth = len(dth_data['ctry_lbl']), dates_label_len_dth= len(dth_data['dt_lbl_loc']))
+    ctry_len = []
+    for a in range(0,len(dth_data['ctry_list'])):
+        ctry_len.append(len(dth_data['ctry_pt'][a]))
+    return render_template('deaths.html', dic= dth_data, ctry_count= len(dth_data['ctry_list']), ctry_len= ctry_len,
+                           wld_lbl_len = len(dth_data['wld_lbl']), ctry_lbl_len = len(dth_data['ctry_lbl']),
+                           dt_lbl_len= len(dth_data['dt_lbl_loc']))
 
 @app.route('/recovered', methods=['GET', 'POST'])
 def foo2():
     rcv_data = rcv.csv()
-    return render_template('recovered.html', dic= rcv_data, len=len(rcv_data['br_pt']), world_label_len = len(rcv_data['wld_lbl']),
-                           country_label_len = len(rcv_data['ctry_lbl']), dates_label_len= len(rcv_data['dt_lbl_loc']))
-
-@app.route('/', methods=['GET', 'POST'])
-def foo3():
-    cs_dataa = cases_par.csv()
-    return render_template('cases2.html', dic= cs_dataa, ctry_count= len(cs_dataa['ctry_list']), ctry_len= cs_dataa['ctry_len'],
-                           wld_lbl_len = len(cs_dataa['wld_lbl']), ctry_lbl_len = len(cs_dataa['ctry_lbl']),
-                           dt_lbl_len= len(cs_dataa['dt_lbl_loc']))
+    ctry_len = []
+    for a in range(0,len(rcv_data['ctry_list'])):
+        ctry_len.append(len(rcv_data['ctry_pt'][a]))
+    return render_template('recovered.html', dic= rcv_data, ctry_count= len(rcv_data['ctry_list']), ctry_len= ctry_len,
+                           wld_lbl_len = len(rcv_data['wld_lbl']), ctry_lbl_len = len(rcv_data['ctry_lbl']),
+                           dt_lbl_len= len(rcv_data['dt_lbl_loc']))
 
 if __name__ == '__main__':
     app.run(debug=True)
